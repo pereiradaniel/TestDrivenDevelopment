f8fbc7a4afaea87341d4508e90f7fbc18f077ad2
Daniel <truenorthtrainingcompany@gmail.com>
2016-09-27 16:54:09 -0400

    Part 1

---
+++ test/models/post_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "should be invalid without a title" do
+    posts(:one).title = nil
+    assert_nil posts(:one).title
+    assert_equal false, posts(:one).valid?
+  end
+
+  test "should be invalid without a body" do
+    posts(:one).body = nil
+    assert_nil posts(:one).body
+    assert_equal false, posts(:one).valid?
+  end
+end
